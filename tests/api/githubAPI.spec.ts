8e67c61d7f4ba912c8745dd8c53072ad981e5a6f
Karlo <krlbrcln12@gmail.com>
2023-10-30 00:24:40 +0800

    debugging

--- tests/api/githubAPI.spec.ts
+++ tests/api/githubAPI.spec.ts
@@ -13,10 +13,10 @@ const USER = 'LordKarlito';
 
 test.describe.configure({mode: 'serial'})
 
-test.beforeAll(async ({ request }) => {
+test.beforeAll(async ({ request, baseURL }) => {
     // console.log(`${process.env.GITSECRET} ${process.env.EMAIL} ${process.env.PASS}`)
     // CREATE A NEW REPO
-    const response = await request.post(`user/repos`, {
+    const response = await request.post(`${baseURL}/user/repos`, {
         data: {
             name: REPO,
         }
@@ -25,14 +25,14 @@ test.beforeAll(async ({ request }) => {
     await expect(response.ok()).toBeTruthy();
 });
 
-test.afterAll(async ({ request }) => {
+test.afterAll(async ({ request, baseURL }) => {
     // DELETE THE REPO
-    const response = await request.delete(`/repos/${USER}/${REPO}`);
+    const response = await request.delete(`${baseURL}/repos/${USER}/${REPO}`);
     await expect(response.ok()).toBeTruthy();
 })
 
-test('Create a bug report', async ({ request }) => {
-    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`,{
+test('Create a bug report', async ({ request, baseURL }) => {
+    const newIssue = await request.post(`${baseURL}/repos/${USER}/${REPO}/issues`,{
         data: {
             title: '[Bug] report 1',
             body: 'Bug description',
@@ -41,7 +41,7 @@ test('Create a bug report', async ({ request }) => {
 
     expect(newIssue.ok()).toBeTruthy();
 
-    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    const issues = await request.get(`${baseURL}/repos/${USER}/${REPO}/issues`);
     await expect(issues.ok()).toBeTruthy();
 
     // For arrays and objects, use expect.objectContaining({someObject}) 
@@ -52,8 +52,8 @@ test('Create a bug report', async ({ request }) => {
     }));     
 });
 
-test('should create a feature request', async ({ request }) => {
-    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+test('should create a feature request', async ({ request, baseURL }) => {
+    const newIssue = await request.post(`${baseURL}/repos/${USER}/${REPO}/issues`, {
     data: {
         title: '[Feature] request 1',
         body: 'Feature description',
@@ -61,7 +61,7 @@ test('should create a feature request', async ({ request }) => {
     });
     await expect(newIssue.ok()).toBeTruthy();
 
-    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    const issues = await request.get(`${baseURL}/repos/${USER}/${REPO}/issues`);
     expect(issues.ok()).toBeTruthy();
     expect(await issues.json()).toContainEqual(expect.objectContaining({
         title: '[Feature] request 1',
